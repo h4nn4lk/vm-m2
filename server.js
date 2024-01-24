5d28c498fce060b832d3d3a9e2bf4c925ddefb24
h4nn4lk <145554031+h4nn4lk@users.noreply.github.com>
2024-01-24 15:34:16 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const nunjucks = require('nunjucks')
+const app = express();
+const port = 3000;
+nunjucks.configure('views', {
+    autoescape: true,
+express: app, 
+}),
+
+app.get('/', (req, res) => {
+  res.render('index.njk');
+  //console.log('someone visited')
+});
+
+app.get('/page2', (req, res) => {
+  res.render('page2.njk');
+  //console.log('someone visited')
+});
+
+app.get('/form', (req, res) => {
+    res.render('form.njk');
+    console.log(req.query)
+    //console.log('someone visited')
+  });
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
