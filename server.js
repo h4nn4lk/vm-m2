8b5287187dc04c5407ed0581a9265dd2132ef7c0
h4nn4lk <145554031+h4nn4lk@users.noreply.github.com>
2024-01-31 14:46:01 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,27 +2,44 @@ const express = require('express');
 const nunjucks = require('nunjucks')
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({
+  extended:true
+}));
+
+
 nunjucks.configure('views', {
     autoescape: true,
-express: app, 
-}),
+    express: app
+});
 
 app.get('/', (req, res) => {
   res.render('index.njk');
-  //console.log('someone visited')
+  //console.log('somebody visited');
 });
 
 app.get('/page2', (req, res) => {
-  res.render('page2.njk');
-  //console.log('someone visited')
+    res.render ('page2.njk');
 });
 
 app.get('/form', (req, res) => {
-    res.render('form.njk');
-    console.log(req.query)
-    //console.log('someone visited')
-  });
-  
+  console.log(req.query);
+  res.render ('form.njk', req.query);
+});
+
+
+app.get('/circle', (req, res) => {
+  console.log(req.query);
+  res.render ('circle.njk', req.query);
+});
+
+app.post('/circle', (req, res) => {
+  let area = Math.PI * req.body.radius * req.body.radius;
+  let diameter = 2 * Math.PI * req.body.radius;
+  let volume = 4/3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
+  res.render('circleAnswer.njk',{r: req.body.radius, a: area, d: diameter, v: volume });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
