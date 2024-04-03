481e525c67b3155e81208f26032f0a59bef8937b
h4nn4lk <145554031+h4nn4lk@users.noreply.github.com>
2024-04-03 13:45:02 +0300

    Add files via upload

--- models/authController.js
+++ models/authController.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
-router.get('/register',async (req, res) =>{
+const User = require('./models/User.js')
+router.get('/register', async (req, res) => {
     res.render('auth/register.njk')
 });
-router.post('/register',async (req, res) =>{
+router.post('/register', async (req, res) => {
     let user = await User.findOne({
-    where: {
-            email:req.body.email
-    }
+        where: {
+            email: req.body.email
+        }
     });
-    if(req.body.pasword !== req.body.password_confirm || user){
+    if (req.body.pasword !== req.body.password_confirm || user) {
         res.redirect('/register');
-    } else{
+    } else {
         User.create({
             name: req.body.name,
             email: req.body.email,
@@ -19,5 +21,24 @@ router.post('/register',async (req, res) =>{
         })
         res.redirect('/')
     }
+});
+
+router.get('/login', async (req, res) => {
+    res.render('auth/login.njk')
+});
+router.post('/login', async (req, res) => {
+    let user = await User.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+    if (!user || !bcrypt.compareSync(req.body.password, user.password)) {
+        res.redirect('/login');
+
+    } else{
+        req.session.user = user;
+        res.redirect('/')
+    }
+
 });
 module.exports = router;
